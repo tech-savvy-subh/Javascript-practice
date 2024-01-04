01a0f65da1cd9aa1176abd7836543436091dfa76
Jishu <jishumandal003@gmail.com>
2024-01-04 14:06:48 +0530

    added operators in prefixpostfix

--- prefixpostfix.js
+++ prefixpostfix.js
@@ -7,3 +7,23 @@ let a=9
 let b=++a
 console.log(a)//in case of prefix first it will increase the value of a to 10 
 console.log(b)//so b will also become 10 in case of prefix 
+
+//operators 
+
+// equality check(==) and comparisons (>,<,>=,<= work differently , comparison converts null toa number treating it as 0)
+console.log(null>0) //false
+console.log(null>=0) //true
+console.log(null<0) //false
+console.log(null<=0) //true
+//these two does not 
+console.log(null==0) //false
+console.log(null===0) //false
+//for undefined it does not 
+console.log(undefined>0) //false
+console.log(undefined>=0) //false
+console.log(undefined<0) //false
+console.log(undefined<=0) //false
+console.log(undefined==0) //false
+console.log(undefined===0)//false
+
+
